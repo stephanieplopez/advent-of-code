2053e67c7956bca3e0199f57618ac52bf8982b2c
JasonMann30 <jason.mann30@gmail.com>
2022-08-26 11:57:18 -0400

    Day One solved

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,7 +6,7 @@ package org.example;
  */
 public class App 
 {
-    public static void main( String[] args )
+    //public static void main( String[] args )
     {
         System.out.println( "Hello World B. Free!");
     }
--- src/main/java/org/example/DayOne.java
+++ src/main/java/org/example/DayOne.java
@@ -1,20 +1,47 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DayOne {
 
-    public void solveDayOne(final String args[]) {
+    public static void main(final String args[]) {
         List<Integer> measurements = new ArrayList<>();
-        List<Integer> increases = new ArrayList<>();
+        //List<Integer> increases = new ArrayList<>();
+
+        try {
+            String strCurrentLine;
+            BufferedReader objReader = null;
+
+            //reads file in
+            objReader = new BufferedReader(new FileReader("/Users/jasonmann/IdeaProjects/advent-of-code/src/main/resources/input.txt"));
+
+            while ((strCurrentLine = objReader.readLine()) != null) {
+                measurements.add(Integer.parseInt(strCurrentLine));
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         int count = 0;
-        for (int i = 0; i <= measurements.size(); i++) {
-            //compare index 0 and index 1 of measurements list
-            //if index 1 > index 0 then increase our count
-            //return our count
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i <= measurements.size() - 2; i++) {
+            a = measurements.get(i);
+            b = measurements.get(i + 1);
+
+            if(b > a) {
+                count++;
+            }
         }
+        System.out.println(count);
     }
 
 
