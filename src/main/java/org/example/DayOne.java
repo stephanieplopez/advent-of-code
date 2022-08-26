645422f55838aa0f525afd568b486cbf10ea0aa5
JasonMann30 <jason.mann30@gmail.com>
2022-08-26 11:19:37 -0400

    DayOne started

---
+++ src/main/java/org/example/DayOne.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DayOne {
+
+    public void solveDayOne(final String args[]) {
+        List<Integer> measurements = new ArrayList<>();
+        List<Integer> increases = new ArrayList<>();
+
+        int count = 0;
+        for (int i = 0; i <= measurements.size(); i++) {
+            //compare index 0 and index 1 of measurements list
+            //if index 1 > index 0 then increase our count
+            //return our count
+        }
+    }
+
+
+}
+
